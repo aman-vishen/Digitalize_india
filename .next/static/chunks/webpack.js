f48801ed4646e0ec05702acb8ff4437f639ad181
aman-vishen <amanvishen46@gmail.com>
2023-07-26 02:49:38 +0530

    added sitemap

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac58aea5428b1ce6"; }
+/******/ 		__webpack_require__.h = function() { return "d476964ec0f3062e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
